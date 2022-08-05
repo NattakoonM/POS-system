d4bd400ce4b002e96f721b33d08ad175d6f1c2cf
NattakoonM <nattakoon110@gmail.com>
2022-08-05 16:14:33 +0700

    layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Row,Col, Navbar, } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      
+        <Row  >
+          <Col md={8} sm={6}  style={{border:'1px dashed',height:'100vh' }} fluid >
+            <Navbar style={{backgroundColor: '#2E3B4C',color:'#FFFDFD',height:'8vh'}} >Product list</Navbar>
+          </Col>
+          <Col md={4} sm={6}  style={{border:'1px dashed',height:'100vh',backgroundColor:'#B8B8B8'}}>
+            <Navbar style={{backgroundColor: '#180E19',color:'#FFFDFD',height:'8vh'}} fluid>Cart list</Navbar>
+          </Col>
+        </Row>
+      
     </div>
   );
 }
