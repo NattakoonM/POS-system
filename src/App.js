8641a4d60a9cd1b7051bf4ecbfb5682d40bf204c
NattakoonM <nattakoon110@gmail.com>
2022-08-05 16:28:37 +0700

    layout-2

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
     <div >
       
         <Row  >
-          <Col md={8} sm={6}  style={{border:'1px dashed',height:'100vh' }} fluid >
+          <Col md={8} sm={6} className='pe-0' style={{height:'100vh' }}  >
             <Navbar style={{backgroundColor: '#2E3B4C',color:'#FFFDFD',height:'8vh'}} >Product list</Navbar>
           </Col>
-          <Col md={4} sm={6}  style={{border:'1px dashed',height:'100vh',backgroundColor:'#B8B8B8'}}>
-            <Navbar style={{backgroundColor: '#180E19',color:'#FFFDFD',height:'8vh'}} fluid>Cart list</Navbar>
+          <Col md={4} sm={6} className='px-0' style={{height:'100vh',backgroundColor:'#B8B8B8'}}>
+            <Navbar style={{backgroundColor: '#180E19',color:'#FFFDFD',height:'8vh'}} >Cart list</Navbar>
           </Col>
         </Row>
       
