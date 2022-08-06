25c0fb89f730ce6c009240b3596ee35a38897b0c
NattakoonM <nattakoon110@gmail.com>
2022-08-06 17:32:36 +0700

    layout2

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Row,Col, Navbar, } from 'react-bootstrap';
+import {Row,Col, Navbar, Container,  } from 'react-bootstrap';
 
 function App() {
   return (
@@ -7,10 +7,26 @@ function App() {
       
         <Row  >
           <Col md={8} sm={6} className='pe-0' style={{height:'100vh' }}  >
-            <Navbar style={{backgroundColor: '#2E3B4C',color:'#FFFDFD',height:'8vh'}} >Product list</Navbar>
+            <Navbar style={{backgroundColor: '#2E3B4C',height:'8vh'}} >
+              
+                <Container  className='d-flex justify-content-center'>
+                  <Navbar.Brand style={{color:'#FFFDFD'}}>
+                  Product list
+                  </Navbar.Brand>
+                </Container>
+            
+            </Navbar>
           </Col>
           <Col md={4} sm={6} className='px-0' style={{height:'100vh',backgroundColor:'#B8B8B8'}}>
-            <Navbar style={{backgroundColor: '#180E19',color:'#FFFDFD',height:'8vh'}} >Cart list</Navbar>
+            <Navbar style={{backgroundColor: '#180E19',height:'8vh'}}>
+              
+                <Container  className='d-flex justify-content-center'>
+                  <Navbar.Brand style={{color:'#FFFDFD'}}>
+                  Cart list
+                  </Navbar.Brand>
+                </Container>
+              
+            </Navbar>
           </Col>
         </Row>
       
